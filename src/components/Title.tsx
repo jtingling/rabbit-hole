8181b8aa19f817be1860448ecd11fae7185a17b4
Jacob Tingling <jacob.tingling@protonmail.com>
2021-08-10 16:38:21 -0400

    Added search feature.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,15 +1,22 @@
-import React, { ReactPropTypes } from 'react';
+import React, { Dispatch, FormEventHandler, ReactPropTypes, SetStateAction } from 'react';
 import { useEffect, useState } from 'react'
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux'
+import { getQuery, getUrl } from '../features/articles/searchSlice';
+import { updateQuery } from '../features/articles/searchSlice'
 import '../styles/layout.css'
 import '../styles/animation.css'
 import Chips from './Chips'
 
 interface Props {
-    menuRef: React.MutableRefObject<HTMLElement | null>,
+    menuRef: React.MutableRefObject<HTMLElement | null>
 }
-const Title: React.FunctionComponent<Props> = ({menuRef}) => {
-    const [ isOpen, setIsOpen] = useState(false);
+const Title: React.FunctionComponent<Props> = ({ menuRef }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const dispatch = useDispatch();
+    const searchWord = useSelector(getQuery);
+    const searchType = useSelector(getUrl);
+
     function toggleMenu(element: React.MutableRefObject<HTMLElement | null>): void {
         if (element.current !== null) {
             if (isOpen) {
@@ -19,25 +26,32 @@ const Title: React.FunctionComponent<Props> = ({menuRef}) => {
                 element.current.style.width = "200px";
                 setIsOpen(true)
             }
-            console.log(isOpen)
         }
-        
     }
+
     return (
-        <>
+        <> 
             <div className="title-ribbon">
-                <button id="title-menu" type='button' onClick={()=> toggleMenu(menuRef)}>menu</button>
+                <button id="title-menu" type='button' onClick={() => toggleMenu(menuRef)}>menu</button>
                 <div className="title-layout">
                     <h1>Rabbit Hole</h1>
                     <div className='title-search'>
-                        <button type='submit'>Search</button>
-                        <input type="search" placeholder="Dive into a new Rabbit Hole." autoFocus />
+                        <form>
+                            <button type='submit' onClick={(e) => {e.preventDefault(); dispatch(updateQuery(searchValue)); setSearchValue("")}}>Search</button>
+                            <input
+                                type="search"
+                                value={searchValue}
+                                onChange={(e) => setSearchValue(e.target.value)}
+                                placeholder="Dive into a new Rabbit Hole."
+                                autoFocus />
+                        </form>
+
                     </div>
                 </div>
                 <button id="title-login" type='submit'>Log In</button>
             </div>
             <div className='chips-layout'>
-                <Chips/>
+                <Chips />
             </div>
         </>
 
