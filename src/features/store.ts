7606d0830f402344125f11cf6995ad86c6cee297
Jacob Tingling <jacob.tingling@protonmail.com>
2021-08-10 16:37:46 -0400

    Added reducers to data store.

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import listsReducer  from './lists/listSlice'
 import UsersReducer from './users/usersSlice'
+import searchSlice from './articles/searchSlice'
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
         lists: listsReducer,
-        users: UsersReducer
+        users: UsersReducer,
+        search: searchSlice
     }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
