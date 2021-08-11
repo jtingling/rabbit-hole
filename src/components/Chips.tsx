44712a092de8482dd3e9efbcb7bd48a7d9e822e1
Jacob Tingling <jacob.tingling@protonmail.com>
2021-08-11 15:57:28 -0400

    Added chip component for similar keywords.

--- src/components/Chips.tsx
+++ src/components/Chips.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { AxiosResponse } from 'axios'
 import '../styles/layout.css'
+import { querySearch } from '../adapters';
+import { getQuery, getSimilarUrl, getUrl } from '../features/articles/searchSlice';
 
-export default function Chips() {
-    
+interface IKeywords {
+    name: string,
+    score: number
+}
+
+const Chips: React.FC = () => {
+    const [keywords, setKeywords] = useState<IKeywords[]>([])
+    let searchWord = useSelector(getQuery);
+    let searchUrl = useSelector(getUrl);
+    let similarUrl = useSelector(getSimilarUrl);
+    let dispatch = useDispatch()
+    useEffect(()=>{
+        ( async () => {
+            let response: AxiosResponse<any> = await querySearch(searchWord, similarUrl);
+            setKeywords(response.data)
+        })()
+    }, [searchWord, searchUrl, dispatch])
+
+    const renderChips = () => {
+        if (keywords.length > 0) {
+            return (
+                keywords.map((word, idx) => {
+                    if (idx < 10) {
+                        return <button className='chip'>{word.name}</button>
+                    }
+                })
+            )
+        } else {
+            return <></>
+        }
+    }
     return (
-        <button className='chip'>chip names</button>
+        <div className='chips-layout'> {console.log(searchWord, similarUrl)}
+        {renderChips()}
+        </div>
     )
 }
+
+export default Chips;
