5aefba3fc8ea907e99d5048eeb16a14c35ecd7b0
Jacob Tingling <jacob.tingling@protonmail.com>
2021-08-13 16:28:46 -0400

    Updated to now close after click.

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,15 +1,15 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateUrl, getUrl } from '../features/articles/searchSlice';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateUrl } from '../features/articles/searchSlice';
 import '../styles/layout.css';
 
 interface Props {
     subMenu: React.MutableRefObject<HTMLDivElement | null>
 }
-const ActionButton: React.FC<Props> = ({subMenu }) => {
-    const [ isOpen, setIsOpen] = useState(false);
+
+const ActionButton: React.FC<Props> = ({ subMenu }) => {
+    const [isOpen, setIsOpen] = useState(false);
     const dispatch = useDispatch();
-    const searchUrl = useSelector(getUrl);
     const toggleButtons = (element: React.MutableRefObject<HTMLDivElement | null>): void => {
         if (element.current !== null) {
             if (isOpen) {
@@ -25,8 +25,16 @@ const ActionButton: React.FC<Props> = ({subMenu }) => {
         <div className='button-container'>
             <button className='action-button' onClick={() => toggleButtons(subMenu)}>+</button>
             <div className='subMenu-buttons' ref={subMenu}>
-                <button id="web" onClick={()=> dispatch(updateUrl('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI'))}>Web</button>
-                <button id="news" onClick={()=>dispatch(updateUrl('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI'))}>News</button>
+                <button id="web" onClick={() => {
+                    dispatch(updateUrl('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI'));
+                    toggleButtons(subMenu)
+                }}>Web
+                </button>
+                <button id="news" onClick={() => {
+                    dispatch(updateUrl('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI'));
+                    toggleButtons(subMenu)
+                }}>News
+                </button>
             </div>
         </div>
 
