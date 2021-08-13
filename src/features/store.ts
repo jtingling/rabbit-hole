acf049ba2d4a9280a412ffade3c9d09f3491051d
Jacob Tingling <jacob.tingling@protonmail.com>
2021-08-13 16:26:40 -0400

    Added slice to store for new feature.

--- src/features/store.ts
+++ src/features/store.ts
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import articleSlice  from './articles/articleSlice'
 import UsersReducer from './users/usersSlice'
 import searchSlice from './articles/searchSlice'
-
+import historySlice from './history/historySlice'
 export const store = configureStore({
     reducer: {
         article: articleSlice,
         users: UsersReducer,
-        search: searchSlice
+        search: searchSlice,
+        history: historySlice,
     }
 })
 
