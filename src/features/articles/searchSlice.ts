7b3be88c095346480724053dad9607a6e7c92a75
Jacob Tingling <jacob.tingling@protonmail.com>
2021-08-13 16:25:31 -0400

    Slice logic to facilitate saved searches.

--- src/features/articles/searchSlice.ts
+++ src/features/articles/searchSlice.ts
@@ -1,12 +1,13 @@
 import { createSlice, PrepareAction, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from '../store';
-interface searchSlice {
+
+export interface Search {
     query: string,
     url: string,
     similarUrl: string,
 }
 
-const initialState: searchSlice = {
+const initialState: Search = {
     query: '',
     url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI',
     similarUrl: 'https://keyword-analysis.p.rapidapi.com/api/query/SimilarQueries'
@@ -16,19 +17,20 @@ const searchSlice = createSlice({
     name: 'search',
     initialState,
     reducers: {
-        query(state: searchSlice, action: PayloadAction<searchSlice>) {
+        query(state: Search, action: PayloadAction<Search>) {
             const { query, url } = action.payload
             state.query = query;
             state.url = url;
         },
-        updateUrl(state: searchSlice, action: PayloadAction<string>) {
+        updateUrl(state: Search, action: PayloadAction<string>) {
             state.url = action.payload;
         },
-        updateQuery(state: searchSlice, action: PayloadAction<string>) {
+        updateQuery(state: Search, action: PayloadAction<string>) {
             state.query = action.payload;
         }
     }
 })
+
 export const { query, updateUrl, updateQuery } = searchSlice.actions;
 export const getQuery = (state: RootState ) => state.search.query;
 export const getUrl = (state: RootState) => state.search.url;
