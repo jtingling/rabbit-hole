ad6b96e83c87ce74a07326b1166ec2f7f391e9ed
Jacob Tingling <jacob.tingling@protonmail.com>
2021-08-24 17:14:44 -0400

    Added infinite scroll feature.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState, useRef, SetStateAction } from 'react'
+import { useEffect, useState, useRef  } from 'react'
 import { searchWeb } from './adapters/webSearch'
 import { useDispatch, useSelector } from 'react-redux'
 import { Switch, Route } from 'react-router-dom';
-import { AxiosResponse } from 'axios'
 import Card from './components/Card';
 import Title from './components/Title';
 import SideBar from './components/SideBar'
@@ -13,56 +12,107 @@ import magnifySVG from './images/magnify-glass.svg';
 import SavedResults from './pages/SavedResults';
 import PinnedQueries from './pages/PinnedQueries';
 import SaveButton from './components/SaveButton';
-import { parseISO } from 'date-fns/esm';
+import _ from 'lodash'
 
 
 const App: React.FC = () => {
-  
+
   const [responseData, setResponseData] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [ error, setError ] = useState<boolean>(false);
+  const [pageNumber, setPageNumber] = useState<number>(1)
   const sideBarRef = useRef<HTMLElement>(null);
   const subMenuRef = useRef<HTMLDivElement>(null);
+  const cardContainerRef = useRef<any>(null);
+  const loadingRef = useRef<any>(null);
   const dispatch = useDispatch();
   const searchWord = useSelector(getQuery);
   const searchType = useSelector(getUrl);
 
-  useEffect(() => {
-    if (searchWord !== "") {
-      (async () => {
-        let response: AxiosResponse<any> = await searchWeb(searchWord, searchType)
-        const date = new Date().toString();
-        const reducer: IQueryData = {
-          searchWord: searchWord,
-          date: date,
-          searchType: searchType
+  const displayRef = {display: isLoading ? "block" : "none"}
+
+
+  useEffect(()=>{
+    window.onscroll = _.debounce(async()=>{
+      let data = responseData;
+      let page = pageNumber
+      if (error || isLoading) return;
+      console.log(page);
+      console.log(Math.round(window.innerHeight), Math.round(document.documentElement.scrollTop), document.documentElement.offsetHeight)
+      if (Math.round((window.innerHeight + document.documentElement.scrollTop) - 40) === document.documentElement.offsetHeight) { //nav has fixed position and must be subtracted for equality
+        page++
+        console.log("match")
+        try {
+          setIsLoading(true);
+          let response: any = await searchWeb(searchWord, searchType, page);
+
+          response.data.value.forEach((result: any)=>{
+            data.push(result);
+          })
+          console.log(data);
+          setResponseData(data);
+          setPageNumber(page);
+          setIsLoading(false)
+        } catch (e) {
+          setError(true);
+          console.log(e);
         }
-        dispatch(addKeyword(reducer))
-        
-        setResponseData(response.data.value)
       }
-      )()
-    }
+    }, 1000)
+  })
+
+
+
+  useEffect(() => {
+    const loadData = async () => {
+      if (searchWord !== "") {
+        try {
+          setIsLoading(true);
+          let response: any = await searchWeb(searchWord, searchType);
+          const date = new Date().toString();
+          const reducer: IQueryData = {
+            searchWord: searchWord,
+            date: date,
+            searchType: searchType
+          }
+          dispatch(addKeyword(reducer))
+          setResponseData(response.data.value)
+          setIsLoading(false)
+        } catch (e) {
+          setError(true);
+          console.log(e);
+        }
+      }
+    }  
+    loadData();
+    setError(false);
+    setPageNumber(1);
+    window.scrollTo(0,0);
   }, [searchWord, searchType, dispatch])
 
   const renderCards = () => {
-    if (responseData !== "") {
+    let articles;
+    if (responseData.length > 0) {
       console.log(responseData)
-      return responseData.map((result: any) => {
+      articles = responseData.map((result: any) => {
         let placeholder = "";
         result.image.url === "" ? placeholder = magnifySVG : placeholder = result.image.url;
-        return <Card 
-          image={placeholder} 
-          title={result.title} 
-          stub={result.snippet} 
-          publishDate={result.datePublished} 
-          url={result.url} 
+        return <Card
+          image={placeholder}
+          title={result.title}
+          stub={result.snippet}
+          publishDate={result.datePublished}
+          url={result.url}
           id={result.id}
-          children={<SaveButton resultData={result}/>}/>
+          children={<SaveButton resultData={result} />} />
       })
+
     } else {
       return (
         <h5>Search results are shown here.</h5>
       )
     }
+    return articles;
   }
 
   return (
@@ -71,16 +121,19 @@ const App: React.FC = () => {
       <div className='content-container'>
         <Switch>
           <Route path="/" exact>
-            {
-              responseData !== "" && renderCards()
-            }
+            <div className='card-container' ref={cardContainerRef}>
+              {renderCards()}
+            </div>
+            <div ref={loadingRef} className="loading">
+              <span style={displayRef}>Loading...</span>
+            </div>
           </Route>
-          <Route path="/SavedResults" component={SavedResults}/>
-          <Route path="/SearchHistory" component={PinnedQueries}/>
+          <Route path="/SavedResults" component={SavedResults} />
+          <Route path="/SearchHistory" component={PinnedQueries} />
 
         </Switch>
         <SideBar menuRef={sideBarRef} />
-        <ActionButton subMenu={subMenuRef}  />
+        <ActionButton subMenu={subMenuRef} />
       </div>
     </div>
   );
