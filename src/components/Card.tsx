746d9da4aeaef6c449ab389ad589f865f8d60419
Jacob Tingling <jacob.tingling@protonmail.com>
2021-09-08 11:30:19 -0400

    Updated Card UI

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import '../styles/layout.css'
+import '../styles/colors.css'
 import TimeAgo from './TimeAgo'
 
 export interface Props {
@@ -10,19 +11,23 @@ export interface Props {
     publishDate: string;
     url: string;
 }
-const Card: React.FC<Props> = ({ title, stub, image, publishDate, articleId, url, children}) => {
+const Card: React.FC<Props> = ({ title, stub, image, publishDate, articleId, url, children }) => {
+    let titleRef = useRef<HTMLHeadingElement | null>(null)
+
     return (
-        <article key={articleId}>
-            <div>
-                <img src={image} alt={title}/>
-                <div>
-                    <h4><a href={url} target="_blank" rel="noreferrer">{title}</a></h4>
-                    
-                    <TimeAgo timestamp={publishDate} published={"Published "}/>
+        <article key={articleId} className='article-container'>
+            <div className='article-body'>
+                <img src={image} alt={title} />
+                <div className='animate-container'>
+                    <h4 ref={titleRef}><a href={url} target="_blank" rel="noreferrer">{title}</a></h4>
+                    <TimeAgo timestamp={publishDate} published={"Published "} />
+                    <p>{stub}</p>
                 </div>
-                <p>{stub}</p>
+                <p className='card-children-container'>
+                        {children}
+                    </p>
             </div>
-            {children}
+
         </article>
     )
 }
