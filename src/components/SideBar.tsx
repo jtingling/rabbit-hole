a1408c556536ceb7136c8de6bd7091ad473d1593
Jacob Tingling <jacob.tingling@protonmail.com>
2021-09-24 15:23:53 -0400

    Moved google sign in to menu

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
-
-
+import Authorisation from './Authorisation';
 
 import ToggleSwitch from './ToggleSwitch';
 
 interface Props {
     menuRef: React.MutableRefObject<HTMLElement | null>,
+    loginRef: React.MutableRefObject<HTMLDivElement | null>,
+    logoutRef: React.MutableRefObject<HTMLButtonElement | null>,
 }
-const SideBar: React.FunctionComponent<Props> = ({ menuRef }) => {
+const SideBar: React.FunctionComponent<Props> = ({ menuRef, loginRef, logoutRef }) => {
     const [isChecked, setIsChecked] = useState(false);
 
-
     const toggleSearchType = (checked: boolean) => {
-        console.log(checked)
         setIsChecked(checked)
     }
     return (
@@ -21,10 +20,12 @@ const SideBar: React.FunctionComponent<Props> = ({ menuRef }) => {
             <div className='menu-container'>
                 <h2>Menu</h2>
                 <ul>
-                    <li><Link to="/">Search</Link></li>
+                    <Link to="/"><li>Search</li></Link>
                     <li><Link to="/SavedResults">Saved Results</Link></li>
                     <li><Link to="/SearchHistory">Search History</Link></li>
+                    <li><Authorisation logoutRef={logoutRef} loginRef={loginRef} /></li>
                 </ul>
+
                 <h4 className='settings'>Settings</h4>
                 <ToggleSwitch name="search" id='SearchType' checked={isChecked} onChange={toggleSearchType} />
             </div>
